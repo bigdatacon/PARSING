32e21b3366c7dd93eb7044c3cf5de7e44fb97b77
Egor <vin17@list.ru>
2019-11-10 20:28:11 +0300

    ДЗ 7 Доработка

--- Lesson7.py
+++ Lesson7.py
@@ -8,31 +8,53 @@ import time
 driver = webdriver.Chrome()
 driver.get('https://www.mvideo.ru/televizory-i-video')
 time.sleep(5)
-
-###  Когда входишь на страницу через хромдрайвер опявлется всплывающее окно его можно закрыть 2 спосоваби:
-### - крестиком, данная кнопка имеет clsaa =button = driver.find_element_by_class_name('PushTip-close')
-### - через кнопку понятно - button = driver.find_element_by_class_name('PushTip-button')
-#### в итоге у меня так и не удалось закрыть всплывающее окно ни через поиск по классу ни черех хпас- button = driver.find_element_by_xpath('//div[@class="PushTip-close"]')
-# button = driver.find_element_by_class_name('PushTip-close')
-# button.click()
-#### В общем чтобы продолжнить работать когда окно отркывается просто руками его закрывал
 print('Вход на страницу МВИДЕО')
 
 while True:
     try:
-        button = WebDriverWait(driver,40).until(
+        iframe = driver.find_element_by_class_name('flocktory-widget')
+        driver.switch_to.frame(iframe)
+        button = WebDriverWait(driver,15).until(
             EC.element_to_be_clickable((By.CLASS_NAME, 'PushTip-close'))
         )
         button.click()
+        """После того как перешел на всплывающий фрейм нужно вернуться обратно, пробовал варианты те что ниже
+        но работает почему то только повторный вход на страницу
+        - driver.get('https://www.mvideo.ru/televizory-i-video'), Вы знаете еще другой вариан как можно решить данную 
+        проблему не открывая страницу повторно?"""
+        # driver.switch_to_default_content()
+        # driver.back()
+        driver.get('https://www.mvideo.ru/televizory-i-video')
+        """тут как правило появлется еще 1 всплывающее окно, его тоже закрываем"""
+        """Вот здесь самый сложный нюанс который я не понял: всплывающее окно появляется с задержкой, 
+        соответственно если не нажимать слип, то данные начинают собираться, но почему то собираются только ссылки
+        хотя описание ровно в том же классе что и ссылка но не собирается, и не собирается цена(там разные варианты 
+        классов пробовал. Но если сделать все как надо и сделать слип чтобы окно закрылось и только потом листать страницы
+        то в итоге вообще ничего не собирает, дажы ссылки, но при этом я смотрел код - ничего не поменялось и 
+        ссылки и описание ровно в тех же тегах -Вы могли бы сказать почему так? """
+        # time.sleep(7)
+        while True:
+            try:
+                iframe = driver.find_element_by_class_name('flocktory-widget')
+                driver.switch_to.frame(iframe)
+                button = WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, 'Widget-close'))
+                )
+                #path-1
+                button.click()
+
+                driver.get('https://www.mvideo.ru/televizory-i-video')
+            except Exception as e:
+                print('Не было всплывающего окна 2')
+                break
     except Exception as e:
         print('Не было всплывающего окна')
         break
-### Ограничиавам количество прохоодв не более 3 раз, такак е ясно сколько там товаров и страниц
 
 pages = 1
-while pages<4:
+while pages<6:
     try:
-        button = WebDriverWait(driver,50).until(
+        button = WebDriverWait(driver,30).until(
             # EC.presence_of_element_located((By.CLASS_NAME, 'catalog__pagination-button'))
             EC.element_to_be_clickable((By.CLASS_NAME, 'sel-hits-button-next'))
         )
@@ -44,25 +66,16 @@ while pages<4:
         print(e)
         break
 
+goods = driver.find_elements_by_class_name('product-tile-info')
 
-#### На знаю почему но он выводит только первый элемент, потом пишет что не может алокировать элемент (unable to locate
-#### elevent, но не ясно почему так если в goods уже попадает массив и там уже все элементы по факту есть как тогда
-#### одного может не быть?
-goods = driver.find_elements_by_class_name('accessories-product-list')
 goods_list = []
-goods_dict = {}
+good_dict = {}
 for good in goods:
-    time.sleep(10)
-    # print(good.find_element_by_class_name('sel-product-tile-title').text)
     good_dict['descr'] = good.find_element_by_class_name('sel-product-tile-title').text
     good_dict['link'] = good.find_element_by_class_name('sel-product-tile-title').get_attribute('href')
-    good_dict['price'] = good.find_element_by_class_name('sel-product-tile-price').text
-    goods_list.append(goods_dict)
-
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client['MVIDEO']
-letters = db.letters
+    # good_dict['price'] = good.find_element_by_class_name('sel-product-tile-price').text
+    good_dict['price'] = good.find_element_by_class_name('product-price').text
+    goods_list.append(good_dict)
 
 for i in goods_list:
-    letters.insert_one(i)
+    print(i)
--- Lesson_7_mail.py
+++ Lesson_7_mail.py
@@ -3,25 +3,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome()
-driver.get('https://e.mail.ru/login')
+driver.get('https://mail.ru/')
 # assert "GeekBrains" in driver.title
-assert "Вход - Почта Mail.ru" in driver.title
 
-""""Не смог залогиниться, потому что кнопка для ввода почты имеет код, тот что ниже, но ни один варинант поиска не находит
-окно ввода, ниже приведены те варианты которые пробовал
-окно ввода, ниже приведены те варианты которые пробовал
-<input name="Login" placeholder="Имя аккаунта" autocapitalize="off" autocomplete="username" autocorrect="off" type="text" class="c0155" value="">"""
-
-#Пробовал:
-# elem = driver.find_element_by_css_selector('input.c0155')
-# elem = driver.find_element_by_name("Login")
-
-# elem = driver.find_element_by_class_name('c0155')
-# elen = driver.find_element_by_tag_name(name, "Login")
-# elem = driver.find_element_by_xpath('//input[@class ="c0155"]')
-
-
+elem = driver.find_element_by_id('mailbox:login')
 elem.send_keys('contestbook@mail.ru')
 elem.send_keys(Keys.RETURN)
-assert "Вход - Почта Mail.ru" in driver.title
+assert "Mail.ru: почта, поиск в интернете, новости, игры" in driver.title
+elem = driver.find_element_by_id('mailbox:password')
+elem.send_keys('link1789')
+elem.send_keys(Keys.RETURN)
 
+""" В данно месте начинаются проблемы - вроде на странице очевидно где каждое входящее письмо, соответственно их нужно просто 
+все собрать в 1 список и потом у каждого вытащить ссылку, но список пустой формируется. При этом я проовал сначала в список
+запихнуть верхний див, а в нем уже перебрать все письма и из них вытащить ссылку - но все равно пустой возвращает, Вы могли 
+бы подсказать в чем дело"""
+mesage_links = driver.find_elements_by_class_name('js-letter-list-item')
+print(mesage_links)
+links= []
+for link in mesage_links:
+    links.append(link.get_attribute('href'))
+
+
+for i in links:
+    print(i)
